162788fc93183085605337813ec307785e2964ca
MatisYahoo <zzhozann@gmail.com>
2020-11-01 17:09:31 +0100

    another_class

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -1,6 +1,12 @@
+class AnotherClass {
+  public static void main(String[] args) {
+    MyClass myObj = new MyClass();
+    System.out.println(myObj.x);
+  }
+}
 
 public class MyClass {
-  int x = 7;
+  int x = 13;
 
   public static void main(String[] args) {
     MyClass myObj = new MyClass();
